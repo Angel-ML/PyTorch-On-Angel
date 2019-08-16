5a2b426a8fec7edbd2796d9c85dfbe2b15b2b9a8
leleyu <yulele214@gmail.com>
2019-08-16 19:39:40 +0800

    code clean

--- python/graph/utils/inits.py
+++ python/graph/utils/inits.py
@@ -63,16 +63,3 @@ def ones(tensor):
     # type: (Optional[Tensor])
     if tensor is not None:
         tensor.fill_(1)
-
-
-def reset(nn):
-    def _reset(item):
-        if hasattr(item, 'reset_parameters'):
-            item.reset_parameters()
-
-    if nn is not None:
-        if hasattr(nn, 'children') and len(list(nn.children())) > 0:
-            for item in nn.children():
-                _reset(item)
-        else:
-            _reset(nn)
