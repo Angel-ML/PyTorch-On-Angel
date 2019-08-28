146d144fc3666f3fb2f94028360ef55389e1b0d7
AutuanLiu <autuanliu@163.com>
2019-08-28 12:44:13 +0800

    update

--- python/recommendation/train.py
+++ python/recommendation/train.py
@@ -1,17 +1,17 @@
-# Tencent is pleased to support the open source community by making Angel available.
-#
-# Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
-# compliance with the License. You may obtain a copy of the License at
-#
-# https://opensource.org/licenses/Apache-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed under the License
-# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-# or implied. See the License for the specific language governing permissions and limitations under
-# the License.
-#
+ # Tencent is pleased to support the open source community by making Angel available.
+ #
+ # Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ # compliance with the License. You may obtain a copy of the License at
+ #
+ # https://opensource.org/licenses/Apache-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software distributed under the License
+ # is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ # or implied. See the License for the specific language governing permissions and limitations under
+ # the License.
+ #
 #!/usr/bin/env python
 from __future__ import print_function
 
@@ -46,11 +46,11 @@ size, dim = X.shape
 # model = deepfm.DeepFM(dim, 13, 10, [10, 5, 1])
 model = deepandwide.DeepAndWide(dim, 13, 10, [10, 5, 1])
 # model = attention_net.AttentionNet(dim, 13, 10, [10, 5, 1])
-# model = attention_net_multi_head.AttentionNetMultiHead(dim, 13, 10, 8, 5, 2, [10, 5, 1])
 # model = dcn.DCNet(dim, 13, 10, cross_depth=4, fc_dims=[10, 10, 10])
 # model = pnn.PNN(dim, 13, 10, [10, 5, 1])
 # model = attention_fm.AttentionFM(dim, 13, 10, 10)
 # model = xdeepfm.xDeepFM(dim, 13, 10, [10, 5, 5], [128, 128])
+# model = attention_net_multi_head.AttentionNetMultiHead(dim, 13, 10, 8, 5, 2, [10, 5, 1])
 model.save("savedmodules/DeepAndWide-model.pt")
 model = torch.jit.load("savedmodules/DeepAndWide-model.pt")
 
