588e7605937bfe9c322e18e2f9e5fca29086e55f
ouyangwen-it <ouyangwen21@163.com>
2020-05-06 13:04:06 +0800

    add gpu support.

--- gen_pt_model.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-IMAGE_NAME=pytorch_on_angel:dev
-MODEL_PATH=$1
-
-docker build --target DEV -t ${IMAGE_NAME} .
-echo "***** generating pytorch script model for ${MODEL_PATH}, output in ./dist *****"
-docker run -it --rm -v $(pwd)/${MODEL_PATH}:/model.py -v $(pwd)/dist:/output -w /output ${IMAGE_NAME} python /model.py ${@:2}
