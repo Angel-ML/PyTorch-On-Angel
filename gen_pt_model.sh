72d29aec6d564a91fab974b0004d2f82219e43ad
JeromeYHJ <41501835+JeromeYHJ@users.noreply.github.com>
2021-09-10 15:03:11 +0800

    Update gen_pt_model.sh
    add /python/graph/nn /python/graph/utils

--- gen_pt_model.sh
+++ gen_pt_model.sh
@@ -6,4 +6,4 @@ MODEL_PATH=$1
 
 docker build --target DEV -t ${IMAGE_NAME} .
 echo "***** generating pytorch script model for ${MODEL_PATH}, output in ./dist *****"
-docker run -it --rm -v $(pwd)/${MODEL_PATH}:/model.py -v $(pwd)/dist:/output -w /output ${IMAGE_NAME} python /model.py ${@:2}
+docker run -it --rm -v $(pwd)/python/graph/nn:/nn -v $(pwd)/python/graph/utils:/utils -v $(pwd)/${MODEL_PATH}:/model.py -v $(pwd)/dist:/output -w /output ${IMAGE_NAME} python /model.py ${@:2}
